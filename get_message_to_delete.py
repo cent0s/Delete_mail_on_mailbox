681ee17f4024ea2168472fb354f44dd1b207728d
cent0s <thodtc@gmail.com>
2018-08-01 17:27:57 +0700

    Add files via upload

---
+++ get_message_to_delete.py
@@ -0,0 +1,111 @@
+from apiclient import errors
+import time
+import os
+from threading import Timer
+import sys
+from apiclient.discovery import build
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+
+SCOPES = 'https://mail.google.com/'
+store = file.Storage('token.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('cre.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+    # creds = None
+service = build('gmail', 'v1', http=creds.authorize(Http()))
+
+"""Get a list of Messages from the user's mailbox.
+"""
+
+
+
+def ListMessagesMatchingQuery(service, user_id, query=''):
+  """List all Messages of the user's mailbox matching the query.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    query: String used to filter messages returned.
+    Eg.- 'from:user@some_domain.com' for Messages from a particular sender.
+
+  Returns:
+    List of Messages that match the criteria of the query. Note that the
+    returned list contains Message IDs, you must use get with the
+    appropriate ID to get the details of a Message.
+  """
+  try:
+    response = service.users().messages().list(userId=user_id,
+                                               q=query).execute()
+    messages = []
+    if 'messages' in response:
+      messages.extend(response['messages'])
+
+    while 'nextPageToken' in response:
+      page_token = response['nextPageToken']
+      response = service.users().messages().list(userId=user_id, q=query,
+                                         pageToken=page_token).execute()
+      messages.extend(response['messages'])
+
+    return messages
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error
+
+
+def ListMessagesWithLabels(service, user_id, label_ids=[]):
+  """List all Messages of the user's mailbox with label_ids applied.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    label_ids: Only return Messages with these labelIds applied.
+
+  Returns:
+    List of Messages that have all required Labels applied. Note that the
+    returned list contains Message IDs, you must use get with the
+    appropriate id to get the details of a Message.
+  """
+  try:
+    response = service.users().messages().list(userId=user_id,
+                                               labelIds=label_ids).execute()
+    messages = []
+    if 'messages' in response:
+      messages.extend(response['messages'])
+
+    while 'nextPageToken' in response:
+      page_token = response['nextPageToken']
+      response = service.users().messages().list(userId=user_id,
+                                                 labelIds=label_ids,
+                                                 pageToken=page_token).execute()
+      messages.extend(response['messages'])
+
+    return messages
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error
+
+
+def DeleteMessage(service, user_id, msg_id):
+  """Delete a Message.
+
+  Args:
+    service: Authorized Gmail API service instance.
+    user_id: User's email address. The special value "me"
+    can be used to indicate the authenticated user.
+    msg_id: ID of Message to delete.
+  """
+  try:
+    service.users().messages().delete(userId=user_id, id=msg_id).execute()
+    print 'Message with id: %s deleted successfully.' % msg_id
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error
+
+if __name__ == '__main__':
+    list_messages = ListMessagesWithLabels(service, 'me', 'Label_5')
+    print list_messages.__len__()
+    DeleteMessage(service,'me',list_messages[0]['id'])
+    # for i in range(len(list_messages)):
+    #     DeleteMessage(service,'me',list_messages[i]['id'])
